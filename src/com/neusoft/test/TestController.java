c66baee15b6f2e42df81d45429238e930b4e2340
RobertLiu123 <1554132204@qq.com>
2018-01-12 13:01:14 +0800

    123

---
+++ src/com/neusoft/test/TestController.java
@@ -0,0 +1,22 @@
+package com.neusoft.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.neusoft.service.IUserService;
+
+@Controller
+public class TestController {
+	@Autowired
+	private IUserService iUserService;
+	@RequestMapping("/")
+	public ModelAndView getUserId(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("1");
+		modelAndView.addObject("lst",iUserService.getUserId());
+		return modelAndView;
+	}
+
+}
